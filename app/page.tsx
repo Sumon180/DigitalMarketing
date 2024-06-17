0c5ebe3a46ae038815cf49c3e55b4fe7fb4550bb
Sumon Hossain <sumonengr.96@gmail.com>
2024-06-17 07:27:56 +0600

    Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import About from "@/components/About";
+import Benifits from "@/components/Benifits";
+import Blog from "@/components/Blog";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Pricing from "@/components/Pricing";
+import Proccess from "@/components/Proccess";
+import Project from "@/components/Project";
 import Services from "@/components/Services";
+import Testimonial from "@/components/Testimonial";
 
 export default function Home() {
   return (
@@ -8,6 +16,14 @@ export default function Home() {
       <Header />
       <Hero />
       <Services />
+      <About />
+      <Proccess />
+      <Testimonial />
+      <Project />
+      <Pricing />
+      <Benifits />
+      <Blog />
+      <Footer />
     </div>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Hero = () => {
-  return <div className="h-screen banner-image bg-cover"></div>;
+  return (
+    <div className="h-screen banner-image bg-cover flex items-center justify-center">
+      <div className="max-w-[1200px] w-full mx-auto">
+        <div className="w-1/2">
+          <p className="uppercase text-sm text-[15px]">
+            <span className="text-[#FFDC60]">//</span>Agency
+          </p>
+          <h1 className="text-[70px] font-bold text-[#292930] leading-[77.35px]">
+            A fully integrated digital{" "}
+            <span className="text-[#5956E8]">marketing</span> agency
+          </h1>
+          <p className="my-5 text-[#555555]">
+            Lorem ipsum dolor sit amet, ea mundi malorum cum, ocurreret
+            reprehendunt ius ex. Ut vis mazim erroribus forensibus.
+          </p>
+          <div className="flex gap-5">
+            <button className="px-5 py-2 rounded bg-[#292930] text-white">
+              Our Services
+            </button>
+            <button className="px-5 py-2 rounded border border-[#292930] text-[#292930]">
+              Get Started
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
